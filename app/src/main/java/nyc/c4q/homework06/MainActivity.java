4973f92025975fe9d21e38b3f3adc4337d860c04
nikunjan <nikunjangc@gmail.com>
2017-10-28 19:07:34 -0400

    test-freeze,layout tags

--- app/src/main/java/nyc/c4q/homework06/MainActivity.java
+++ app/src/main/java/nyc/c4q/homework06/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import java.lang.Math;
 
 
 public class MainActivity extends AppCompatActivity {
